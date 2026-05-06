6d9ec97506a06f35e203e87aa5be3c44ff8638bc
pierdlcr2006 <dpierofabrizzio@icloud.com>
2026-05-05 20:39:44 -0500

    Initial Commit

---
+++ actividad-lab07/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  actividad-lab07
+//
+//  Created by piero on 5/05/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
